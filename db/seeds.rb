82ea40eb35f567fe621d41ddfb9bb2a9737168b4
Coline Forde <coline.forde@gmail.com>
2016-04-01 16:51:09 -0700

    update migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,8 @@
 
 
 User.create!(username: "coline", email: "coline@gmail.com", password: "password")
+User.create!(username: "bob", email: "bob@gmail.com", password: "password")
+Card.create!(greeting: "hello", message: "I miss you!")
+Recipient.create!(recipient_id: 2, sender_id: 1)
+UserCard.create!(user_id: 1, card_id: 1)
+Template.create!(image: "https://robohash.org/my-own-slug.png?size=50x50", card_id: 1)
