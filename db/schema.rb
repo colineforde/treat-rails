cecfc53197884fd7a5f1990cc2b7beb641dafe11
Coline Forde <coline.forde@gmail.com>
2016-04-01 17:08:26 -0700

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160401234008) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cards", force: :cascade do |t|
     t.string   "greeting"
     t.string   "message"
