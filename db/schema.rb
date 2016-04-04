16eae72cc3771701851a7092952377223fc1e0de
Coline Forde <coline.forde@gmail.com>
2016-04-04 12:11:39 -0700

    add user auth

--- db/schema.rb
+++ db/schema.rb
@@ -48,9 +48,9 @@ ActiveRecord::Schema.define(version: 20160401234008) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password_hash"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
