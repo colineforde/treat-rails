8210346eb40e8884153e87ef5ca12ffb4c9b5a11
Coline Forde <coline.forde@gmail.com>
2016-04-03 17:27:37 -0700

    turn card information into json

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -5,6 +5,14 @@ class CardsController < ApplicationController
 	def sent
 		@user = User.first
 		@cards = @user.user_cards
+		card_info = []
+		@cards.each do |card|
+			card_info << ["recipient: #{User.find(card.recipient.id).username}",
+				"greeting: #{card.card.greeting}",
+				"message: #{card.card.message}"
+			]
+		end
+		render json: card_info
 	end
 
 	def show
