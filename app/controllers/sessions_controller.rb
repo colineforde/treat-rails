55cec9f4d9e3911331106bafa970d545af3bb3d4
Coline Forde <coline.forde@gmail.com>
2016-04-04 13:03:57 -0700

    refactor authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,12 @@
 class SessionsController < ApplicationController
 	def create
-		@user = User.find_by(email: params[:email])
-		if @user.authenticate(params[:password]) 
-			session[:user_id] = @user.id
-			render json: @user
+		if @user = User.find_by(email: params[:email])
+			if @user.authenticate(params[:password]) 
+				session[:user_id] = @user.id
+				render json: @user
+			else
+				render json: {authorization: false, message: "invalid password"}
+			end
 		else
 			render json: {authorization: false, message: "user is invalid"}
 		end
